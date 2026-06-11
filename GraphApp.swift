f8c19bd8957ec81791dbfc53118f5472de96ac1d
Amemus79 <karlo@earthlink.net>
2026-06-10 20:16:25 -0400

    Update GraphApp for cross-platform iOS and macOS support

--- GraphApp.swift
+++ GraphApp.swift
@@ -1,10 +1,24 @@
 import SwiftUI
 
-@main
 struct GraphApp: App {
     var body: some Scene {
+        #if os(macOS)
+        WindowGroup {
+            ContentView()
+                .frame(minWidth: 1200, minHeight: 800)
+        }
+        .windowStyle(.hiddenTitleBar)
+        .commands {
+            CommandGroup(replacing: .appInfo) {
+                Button("About Swift Playgrounds Graph") {
+                    // About dialog
+                }
+            }
+        }
+        #else
         WindowGroup {
             ContentView()
         }
+        #endif
     }
 }
